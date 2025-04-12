e282bdd92fddb4c26af892276e2d430a424b2eaa
IbrahimAhmed-IA <ibrahimahmed20u10@gmail.com>
2025-04-12 19:02:41 +0200

    Update task-manager.tsx

--- src/components/task-manager/task-manager.tsx
+++ src/components/task-manager/task-manager.tsx
@@ -82,7 +82,12 @@ export default function TaskManager() {
               Uncheck All Tasks
             </Button>
 
-
+            <Button
+              onClick={sortTasks}
+              className="sm:col-span-2 bg-blue-500 hover:bg-blue-600"
+            >
+              Sort Tasks
+            </Button>
           </div>
         </div>
 
